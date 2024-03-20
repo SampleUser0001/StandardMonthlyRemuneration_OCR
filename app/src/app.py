6da2bd086236aab5e87413c8ed4efcd893ce37dd
SampleUser0001 <ittimfn+github@gmail.com>
2024-03-20 20:26:53 +0900

    作成終了

--- app/src/app.py
+++ app/src/app.py
@@ -9,6 +9,10 @@ from importenv import ImportEnvKeyEnum
 from util.sample import Util
 from controller import SampleController
 
+import pytesseract
+from PIL import Image
+import pandas as pd
+
 PYTHON_APP_HOME = os.getenv('PYTHON_APP_HOME')
 LOG_CONFIG_FILE = ['config', 'log_config.json']
 
@@ -18,9 +22,28 @@ config.dictConfig(log_conf)
 logger.setLevel(DEBUG)
 logger.propagate = False
 
-def sample_func():
-    logger.info('hoge')
-    logger.debug('hoge')
+def convert_to_tsv():
+    
+    # Open the image file
+    import_path = os.path.join(PYTHON_APP_HOME, 'import', 'clipboarded.png')
+    logger.info(f'import_path : {import_path}')
+    img = Image.open(import_path)
+
+    # Use tesseract to do OCR on the image
+    # Tesseractの日本語の言語パックがインストールされていることを確認してください
+    text = pytesseract.image_to_string(img, lang='jpn')
+
+    # The expected format is a TSV, so we need to process the string into lines and columns
+    lines = text.split('\\n')
+    rows = [line.split() for line in lines if line.strip()]  # 空白で区切られた各行をリストに変換
+
+    # Create a DataFrame
+    df = pd.DataFrame(rows)
+
+    # Save to a TSV file
+    tsv_path = os.path.join(PYTHON_APP_HOME, 'export', 'standard_monthly_remunerantion.tsv')  # 出力するTSVファイルのパスに変更してください
+    logger.info(f'tsv_path : {tsv_path}')
+    df.to_csv(tsv_path, sep='\t', index=False, header=False)
 
 if __name__ == '__main__':
     # 起動引数の取得
@@ -28,14 +51,7 @@ if __name__ == '__main__':
     # args[0]はpythonのファイル名。
     # 実際の引数はargs[1]から。
     
-    logger.info('This is logger message!!')
-    logger.debug('This is logger message!!')
-
-    # .envの取得
-    logger.info(f'ImportEnvKeyEnum.SAMPLE.value : {ImportEnvKeyEnum.SAMPLE.value}')
-
-    sample_func()
-
-    Util.print()
-    
-    SampleController().print_log()
+    logger.info('Start')
+    logger.info('Convert to TSV')
+    convert_to_tsv()
+    logger.info('Finish')
